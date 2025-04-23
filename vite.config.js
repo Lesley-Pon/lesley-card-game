721ed3fa7e2498bf31abdc890a0d558599c727d6
Lesley-Pon <lesleypondodo@gmail.com>
2025-04-23 16:57:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // 告訴 Vite 專案根目錄是這個資料夾
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html', // 明確指定 index.html 的位置
+    },
+  },
 })
