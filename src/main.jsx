6b334f499692b0cc1382207dbb6570cc9c8cd121
Lesley-Pon <lesleypondodo@gmail.com>
2025-04-23 16:52:12 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import InterviewCardDrawer from './InterviewCardDrawer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InterviewCardDrawer />
+  </React.StrictMode>
+)
